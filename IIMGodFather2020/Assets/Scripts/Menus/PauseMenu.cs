ec6a37a52d43fa25c4e01097baa78dfcd980fb5f
LaPrisePeritel <LaPrisePeritel@users.noreply.github.com>
2020-09-02 13:13:14 +0200

    Pause Menu

---
+++ IIMGodFather2020/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Menu")]
+    public string menuLevel;
+    public string nameLevel;
+
+    [Header("Canvas")]
+    public Canvas canvas;
+
+    public void Quit()
+    {
+        Debug.Log("Retour au menu principal");
+        SceneManager.LoadScene(menuLevel);
+    }
+
+    public void ResumeGame()
+    {
+        canvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(nameLevel);
+        Time.timeScale = 1;
+    }
+}
