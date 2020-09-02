cc7a21a8c984818e039470d4ddde9ed845fb2281
LaPrisePeritel <LaPrisePeritel@users.noreply.github.com>
2020-09-02 11:36:36 +0200

    Main Menu

---
+++ IIMGodFather2020/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Options")]
+    public AudioMixer audioMixer;
+
+    [Header("Menu")]
+    public string nameLevel;
+    
+    
+    
+    public void SetMenuVolume(float volume)
+    {
+        audioMixer.SetFloat("MainMenuVolume", volume);
+    }
+    
+    public void PlayGame()
+    {
+        Debug.Log("Play !");
+        SceneManager.LoadScene(nameLevel);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit !");
+        Application.Quit();
+    }
+}
