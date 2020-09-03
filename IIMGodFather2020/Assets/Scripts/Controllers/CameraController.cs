84c1ed003e8e6df0545cbfe21a6dff8eb28bf576
Record <record.tristan@gmail.com>
2020-09-03 16:56:15 +0200

    Add bounds of cam to never exit this

--- IIMGodFather2020/Assets/Scripts/Controllers/CameraController.cs
+++ IIMGodFather2020/Assets/Scripts/Controllers/CameraController.cs
@@ -22,6 +22,10 @@ public class CameraController : MonoBehaviour
     public float speedTransitionMovement = 1;
     private float _currentSpeedTransitionMovement = 0;
 
+    [Header("Bounds Cam")]
+    public CameraBounds bounds;
+
+
     private Camera _camera = null;
 
     private void Awake()
@@ -34,12 +38,15 @@ public class CameraController : MonoBehaviour
         _finalZoom = limitMinZoom;
     }
 
+
+    [System.Obsolete("FOV non Size ortho")]
     private void LateUpdate()
     {
         ////Player on viewport
         Vector2 PlayerOnScreen = Camera.main.WorldToViewportPoint(GameController.instance.player.transform.position);
         if (PlayerOnScreen.x < viewportLimit.x || PlayerOnScreen.y < viewportLimit.y || PlayerOnScreen.x > 1 - viewportLimit.x || PlayerOnScreen.y > 1 - viewportLimit.y)
         {
+
             bool up, down, left, right;
             down = PlayerOnScreen.y < viewportLimit.y;
             up = PlayerOnScreen.y > 1 - viewportLimit.y;
@@ -50,20 +57,26 @@ public class CameraController : MonoBehaviour
 
             if (PlayerOnScreen.x < 0.1f || PlayerOnScreen.y < 0.1f || PlayerOnScreen.x > 1 - 0.1f || PlayerOnScreen.y > 1 - 0.1f)
             {
-                down = PlayerOnScreen.y < 0.1f;
-                up = PlayerOnScreen.y > 1 - 0.1f;
-                left = PlayerOnScreen.x < 0.1f;
-                right = PlayerOnScreen.x > 1 - 0.1f;
-
-                if ((directionMovement.x > 0 && left) || (directionMovement.x < 0 && right) || (directionMovement.y > 0 && down) || (directionMovement.y < 0 && up))
+                if (GameController.instance.player.GetVelocity() != Vector2.zero)
                 {
-                    _currentSpeedTransitionMovement = 0;
+                    _currentSpeedTransitionMovement = GameController.instance.player.GetVelocity().magnitude;
                 }
                 else
                 {
-                    _currentSpeedTransitionMovement = speedTransitionMovement;
-                }
+                    down = PlayerOnScreen.y < 0.1f;
+                    up = PlayerOnScreen.y > 1 - 0.1f;
+                    left = PlayerOnScreen.x < 0.1f;
+                    right = PlayerOnScreen.x > 1 - 0.1f;
 
+                    if ((directionMovement.x > 0 && left) || (directionMovement.x < 0 && right) || (directionMovement.y > 0 && down) || (directionMovement.y < 0 && up))
+                    {
+                        _currentSpeedTransitionMovement = 0;
+                    }
+                    else
+                    {
+                        _currentSpeedTransitionMovement = speedTransitionMovement;
+                    }
+                }
             }
             else
             {
@@ -89,16 +102,20 @@ public class CameraController : MonoBehaviour
             _currentSpeedTransitionMovement = speedTransitionMovement;
         }
 
+
         //Use gravity or not
         if (GameController.instance.player.GetVelocity() != Vector2.zero)
         {
             //Change position of camera
             transform.position = Vector2.MoveTowards(transform.position, GameController.instance.player.GetVelocity(), _currentSpeedTransitionMovement * Time.deltaTime);
+            transform.position = ClampPositionToScreen(transform.position);
         }
         else
         {
             //Change position of camera
             transform.position = Vector2.MoveTowards(transform.position, GameController.instance.player.GetEndPositionMovement(), _currentSpeedTransitionMovement * Time.deltaTime);
+            transform.position = ClampPositionToScreen(transform.position);
+
         }
 
         //Smooth zoom
@@ -123,6 +140,31 @@ public class CameraController : MonoBehaviour
         _finalZoom = Mathf.Clamp(_finalZoom, limitMinZoom, limitMaxZoom);
     }
 
+    private Vector3 ClampPositionToScreen(Vector3 position)
+    {
+        Vector3 bottomLeft = _camera.ScreenToWorldPoint(Vector3.zero);
+        Vector3 topRight = _camera.ScreenToWorldPoint(new Vector3(_camera.pixelWidth, _camera.pixelHeight, 0));
+
+        //Calculate screen size
+        Vector2 screenSize = new Vector2(topRight.x - bottomLeft.x, topRight.y - bottomLeft.y);
+        Vector2 halfScreenSize = screenSize / 2;
+
+        //Clamp position
+        position.x = Mathf.Clamp(
+            position.x,
+            bounds.left + halfScreenSize.x,
+            bounds.right - halfScreenSize.x
+        );
+
+        position.y = Mathf.Clamp(
+            position.y,
+            bounds.bot + halfScreenSize.y,
+            bounds.top - halfScreenSize.y
+        );
+
+        return position;
+    }
+
     /// <summary>
     /// Zoom action is to make a zoom to your player
     /// This version is to select a particular strenght
@@ -132,4 +174,38 @@ public class CameraController : MonoBehaviour
         _finalZoom = Strenght;
         _finalZoom = Mathf.Clamp(_finalZoom, limitMinZoom, limitMaxZoom);
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+
+        float gizmosCubeSize = 0.3f;
+
+        Vector3 topLeft = new Vector3(bounds.left, bounds.top, transform.position.z);
+        Gizmos.DrawCube(topLeft, Vector3.one * gizmosCubeSize);
+
+        Vector3 BottomLeft = new Vector3(bounds.left, bounds.bot, transform.position.z);
+        Gizmos.DrawCube(BottomLeft, Vector3.one * gizmosCubeSize);
+
+        Vector3 topRight = new Vector3(bounds.right, bounds.top, transform.position.z);
+        Gizmos.DrawCube(topRight, Vector3.one * gizmosCubeSize);
+
+        Vector3 bottomRight = new Vector3(bounds.right, bounds.bot, transform.position.z);
+        Gizmos.DrawCube(bottomRight, Vector3.one * gizmosCubeSize);
+
+
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomRight, BottomLeft);
+        Gizmos.DrawLine(BottomLeft, topLeft);
+    }
+}
+
+[System.Serializable]
+public class CameraBounds
+{
+    public float top = 1;
+    public float right = 1;
+    public float bot = 1;
+    public float left = 1;
 }
