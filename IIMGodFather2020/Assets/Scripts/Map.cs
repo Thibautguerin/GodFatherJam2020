10d5d9eeb70f2e703c9afa611484326b148f16b1
Record <record.tristan@gmail.com>
2020-09-03 11:09:25 +0200

    Map path

--- IIMGodFather2020/Assets/Scripts/Map.cs
+++ IIMGodFather2020/Assets/Scripts/Map.cs
@@ -11,13 +11,22 @@ public class Map : MonoBehaviour
     public float stepPos;
 
     public List<int[]> map = new List<int[]>();
+    public List<MapShard> allShards = new List<MapShard>();
 
-    public struct MapShard
+    public class MapShard
     {
-        public int sizeX;
-        public int sizeY;
+        public int sizeX = 0;
+        public int sizeY = 0;
 
-        public MapShard[] shardsList;
+        public MapShard[] shardsList = new MapShard[4];
+
+        public List<PlacementPoint> placementPoints = new List<PlacementPoint>();
+
+        public MapShard(int sizeX, int sizeY)
+        {
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+        }
 
     }
 
@@ -34,7 +43,7 @@ public class Map : MonoBehaviour
         int stepCreateX = (int)(sizeX / stepPos);
         int stepCreateY = (int)(sizeY / stepPos);
 
-        
+        //MapShard firstShard =
     }
 }
 
